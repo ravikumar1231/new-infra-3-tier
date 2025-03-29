51ef694e0de0af64dab89fc31777d95a10647018
ravikumar1231 <ravikumardevops1231@gmail.com>
2025-03-29 20:25:28 +0530

    db.tf

--- db.tf
+++ db.tf
@@ -1,5 +1,5 @@
 #### RDS ####
-resource "aws_db_subnet_group" "swiggy-db-sub-grp" {
+/*resource "aws_db_subnet_group" "swiggy-db-sub-grp" {
   name       = "swiggy-db-sub-grp"
   subnet_ids = ["${aws_subnet.swiggy-pvt-sub-3.id}","${aws_subnet.swiggy-pvt-sub-4.id}"]
 }
@@ -24,6 +24,6 @@ resource "aws_db_instance" "swiggy-db" {
     prevent_destroy = true
     ignore_changes  = all
   }
-}
+}*/
 
 
